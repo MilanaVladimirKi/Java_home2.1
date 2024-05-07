08f4673ae9e21e4f9e977efc8a8c22651d5ac9d2
Milana Kichutkina <MilanaVladimir@mail.ru>
2024-05-07 10:51:57 +0500

    Initial commit

---
+++ Home_2.1/src/Main/Main.java
@@ -0,0 +1,16 @@
+package Main;
+
+public class Main {
+    public static void main(String[] args){
+
+        int price = 13676;
+
+        int mills = 20;
+
+        System.out.println("bonus");
+
+        System.out.println(price / mills);
+
+    }
+
+}
